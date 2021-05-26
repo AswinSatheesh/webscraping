b316a52cfe36318ace51a6c54dbc3c0108f52f38
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-05-26 17:03:35 +0530

    code updated

--- webscraping.py
+++ webscraping.py
@@ -1,20 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 soup = BeautifulSoup(
     requests.get("https://medium.com/featurepreneur").content,
     "html.parser")
     
-count=0
-section=soup.find_all('div',class_='postMetaInline postMetaInline-authorLockup ui-captionStrong u-flex1 u-noWrapWithEllipsis')
+# print(soup.prettify)
 
-for sec in section:
-    headline= sec.get_text()
-    print(headline)
+count =0
+
+section=soup.find_all('div',class_="section-inner sectionLayout--insetColumn")
+
+for sc in section:
+    head=sc.get_text()
+    print(head)
     count=count+1
+    print(" ")
 
-print()
-print("Number of Articles : ",count)
+print("Number of Articles : ", count)
+
+
+
+    
+
+
+
 
 
